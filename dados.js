1aab6f710af6fad069a0c1ee7faab5e81afb7b52
Giulia Borges da Silva <giuliaborgesdasilva@gmail.com>
2024-09-16 23:53:57 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,80 @@
+
+let dados = [
+    {
+        titulo: "Chilli de abóbora",
+        descricao: "Chilli de abóbora é uma variação saborosa e nutritiva do clássico chilli. A abóbora adiciona uma doçura natural e cremosidade ao prato, tornando-o perfeito para os dias mais frios.",
+        categoria: "Salgados",
+        tempoPreparo: "2h",
+        ingredientes: ["Abóbora Moranga", "Cebola", "Alho", "Azeite", "Tomate", "Folha de Louro", "Cenoura", "Vinho Branco", "Carne de Porco", "Bacon", "Iogurte Natural Azedo", "Sal", "Especiarias: Canela, Gengibre, Noz Moscada e Cravo", "Pimentas: Pimentão Vermelho, Pimenta Calabresa e Pimenta Chilli"],
+        passos: ["Para melhores efeitos, um dia antes, corte a Carne de Porco em cubos e a deixe marinar no Iogurte e Sal", "Corte a Abóbora, a Cenoura e o Tomate em pedaços"],
+        id: 0
+    },
+    {
+        titulo: "Rondelli de presunto e queijo",
+        descricao: "Rondelli de abobrinha são fatias de abobrinha grelhadas ou assadas, perfeitas para acompanhar carnes ou como prato principal vegetariano.",
+        categoria: "Salgados",
+        tempoPreparo: "55min",
+        ingredientes: ["2 caixinhas de polpa de tomate", "1 xícara de chá de água", "500 g de massa para lasanha", "500 g de presunto fatiado", "500 g de mussarela fatiada", "Orégano a gosto", "Parmesão a gosto"],
+        passos: ["Coloque uma caixinha de molho de tomate e ½ xícara de água em uma tigela, e misture. Se o molho for ralo, não coloque a água. Depois, despeje-o num refratário e reserve;",
+            "Pegue uma folha da massa, e cubra-a com duas fatias de presunto e duas de queijo;",
+            "Faça um rolo, de modo que o presunto e o queijo fiquem dentro da massa;",
+            "Corte o rolo em 3 pedaços iguais e disponha-os no refratário com o molho. Repita o processo até acabar a massa e preencher todo o refratário;",
+            "Misture o outro sachê de molho de tomate com mais ½ xícara de água, despeje por cima dos rondellis, salpique orégano e parmesão;",
+            "Cubra o refratário com papel alumínio, e leve-o ao forno preaquecido a 180 ºC por cerca de 30 minutos. Depois, retire o papel alumínio e deixe assar por mais 10 minutos;",
+            "Agora é só servir. Bom apetite!"],
+    },
+
+    {
+        titulo: "Chicken-Tikka-Massala",
+        descricao: "Chicken-Tikka-Massala é um prato indiano clássico, com pedaços de frango marinados em iogurte e especiarias, assados e finalizados em um molho cremoso de tomate e especiarias.",
+        categoria: "Salgados",
+        tempoPreparo: "4h",
+        ingredientes: ["3 peitos de frango", "1 limão", "1 cebola grande picada", "1 tomate picado", "2 dentes de alho picados", "1 frasco de molho de tomate", "1 pedaço pequeno de gengibre", "1 pote de iogurte natural", "1 lata de creme de leite", "100 ml de água", "½ maço de coentro", "Especiarias em pó: Pimenta do Reino, Cominho, Noz-moscada, Açafrão, Canela, Cardamomo"],
+        passos: ["Depois de cortar os filés de frango em cubos, coloque-os em uma tigela e tempere com sal, limão, as especiarias e o iogurte natural. Deixe marinar na geladeira por, no mínimo, 3 horas. Reserve um pouco das especiarias para uma segunda parte da receita.",
+             "Após a marinada, doure o frango em uma frigideira com azeite. Não precisa dourar muito, pois o frango vai terminar de cozinhar com o molho.", "Reserve o frango.",
+            "Em uma panela, doure a cebola e o alho junto com o gengibre.", "Adicione o tomate e o resto das especiarias na panela, e mexa até o aroma subir.", "Despeje molho de tomate na panela e misture com o refogado.", "Adicione o frango, misture bem e, por fim, adicione a água.","Deixe o frango terminar de cozinhar com o molho.", 
+            "Desligue o fogo e adicione o creme de leite.", "Finalize com o coentro picado."],
+        link: "https://www.hojetemfrango.com.br/receita/frango-ao-molho-tikka-masala/",
+
+    },
+    {
+        titulo: "Cheesecake de Maracujá",
+        descricao: "Uma explosão de sabor em cada garfada! Nosso cheesecake de maracujá combina a cremosidade do queijo com a acidez refrescante da fruta, resultando em uma sobremesa irresistível.",
+        categoria: "Doces",
+        tempoPreparo: "4h40min",
+        ingredientes: ["200g de Biscoito de Maisena", "100g de coco ralado seco e tostado", "100g de manteiga sem sal", "meia xícara (chá) de suco de maracujá (cerca de 3 unidades)", "600g de cream cheese", "3 colheres (sopa) de açúcar refinado", "400g de Chocolate Branco", "1 caixa de Creme de Leite", "polpa de 3 maracujás", "4 colheres (sopa) de açúcar refinado"],
+        passos: ["Para começar com a massa, em um processador, acrescente e processe os Biscoitos de Maisena e o coco ralado. Em seguida, despeje a manteiga derretida sobre essa mistura e processe novamente.", "Forre o fundo e as laterais de uma forma de 21 cm de diâmetro (com fundo removível) com essa farofa úmida e reserve na geladeira.",
+            "Em um liquidificador, acrescente e bata a polpa dos maracujás. Espere alguns minutos para que as sementes se depositem no fundo. Depois, coe o suco - você vai precisar de 3/4 de xícara.",
+            "No processador, junte o suco do maracujá, o açúcar e o cream cheese. Processe.",
+            " Despeje o Chocolate Branco derretido com o Creme de Leite sobre a mistura e processe até obter um creme homogêneo.", "Transfira o creme para a forma, despejando delicadamente sobre a base de biscoitos.",
+            "Alise a superfície com uma espátula e leve à geladeira por 4 horas.",
+            "Para a calda, em uma panela, acrescente a polpa do maracujá e o açúcar e leve para esquentar até que a calda fique mais líquida.",
+            "Despeje a calda sobre a cheesecake na hora de servir e aproveite!"],
+    },
+    {
+        titulo: "Biscoitos de Cacau",
+        descricao: "Macios por dentro e levemente crocantes por fora, biscoitos de cacau são uma verdadeira tentação! Perfeitos para um lanche da tarde acompanhados de um copo de leite gelado ou para presentear alguém especial.",
+        categoria: "Doces",
+        tempoPreparo: "40min",
+        ingredientes: ["1 xícara (chá) de açúcar cristal", "2 ovos", "½ colher (sopa) de fermento em pó", "1 colher (sopa) de cacau em pó", "5 colheres (sopa) de manteiga ou margarina", "5 colheres (sopa) de manteiga ou margarina", "2 e ½ xícaras (chá) de farinha de trigo", "100 g de chocolate meio amargo (opcional)"],
+        passos: ["Preaqueça o forno a 180° C e unte a forma.", "Em um recipiente, adicione o açúcar e a manteiga e mexa até formar uma pasta.", "Em seguida, coloque os ovos e mexa até formar uma massa lisa.", 
+            "Vá adicionando a farinha de trigo até que a massa solte do recipiente e não grude nas mãos.", "Coloque o cacau em pó", "Logo em seguida, adicione o fermento e misture.", "Depois de concluída essas etapas, acrescente o chocolate picado, se preferir com.", "Faça bolinhas, achate-as e coloque-as na forma untada.",
+            "Leve ao forno já preaquecido e aumente a temperatura do forno a 250° C."],
+        link: "https://www.tudogostoso.com.br/receita/198774-cookies-sem-acucar-mascavo.html"
+
+    },
+    {
+        titulo: "Sagu de Vinho",
+        descricao: "Uma viagem no tempo através do sabor! O sagu de vinho é uma tradicional sobremesa brasileira, com um toque especial de vinho tinto que confere um sabor único e sofisticado. Perfeito para aquecer os dias frios e proporcionar momentos de aconchego.",
+        categoria: "Doces",
+        tempoPreparo: "1h",
+        ingredientes: ["2 xícaras de sagu", " 4 xícaras de vinho tinto", "Açúcar a gosto", "6 xícaras de água"],
+        passos: ["Coloque o sagu em uma travessa com a água e deixe de molho por 10 minutos.", "Depois escorra a água do sagu e o coloque para ferver.", 
+            "Depois de 15 minutos, ou assim que algumas bolinhas começarem a ficar transparentes, retire a panela do fogo.",
+            "Despeje o conteúdo da panela em um escorredor de macarrão e os lave para tirar a gosma resultante do cozimento.",
+            "Coloque em outra panela o vinho, o açúcar e uma xícara de água.",
+            "Quando começar a ferver, coloque o sagu e espere cozinhar mais uns 15 minutos, até todas as bolas ficarem transparentes.",
+            "Coloque em um recipiente e deixe esfriar."
+        ]
+    }
+]
